3b96f854c254bd9b318d79ff41499245f877a1ed
zhang tian <975571814@qq.com>
2018-05-16 15:16:15 +0800

    write docker-compose file
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@Configuration
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
